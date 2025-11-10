0d30705bd6700708fed224afc97676fce944e711
Namminu <118587384+Namminu@users.noreply.github.com>
2025-11-10 22:35:52 +0900

    ReSetting

---
+++ Source/Untitled/UntitledGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UntitledGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNTITLED_API AUntitledGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
