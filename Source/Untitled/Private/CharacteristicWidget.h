125376d9431fd35306bb962ae9dcfa494ed4799b
Namminu <118587384+Namminu@users.noreply.github.com>
2025-11-11 21:22:08 +0900

    Widget 세분화
    MainWidget
    - Characteristic Widget
    - Analysis Widget
    - Comparision Widget
    - AIChat Widget
    구분 예정
    
    MainWidget 과 AIChat 구성중

---
+++ Source/Untitled/Private/CharacteristicWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CharacteristicWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UCharacteristicWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Untitled/Private/ComparisionWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ComparisionWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UComparisionWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
